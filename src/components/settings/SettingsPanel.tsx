1171654d1c818a96c2bfaeee2072d103f85080a8
DepleyPuma <szymon.szarek1@wp.pl>
2026-02-25 13:02:34 +0100

    add new section temp

--- src/components/settings/SettingsPanel.tsx
+++ src/components/settings/SettingsPanel.tsx
@@ -26,9 +26,9 @@ export function SettingsPanel({ userFromSupabase }: SettingsPanelProps) {
       <div className="relative flex h-[calc(100vh-85px)] w-1/6 bg-gray-300">
         Sidebar
       </div>
-      <div className="mx-auto max-w-4xl flex-1 p-4 md:p-8">
+      <div className="mx-auto max-w-4xl flex-1 p-4 space-y-8 md:p-8">
+        <h1 className="mb-6 text-4xl font-bold">Ustawienia</h1>
         <section>
-          <h1 className="mb-6 text-4xl font-bold">Ustawienia</h1>
           <div className="mb-4 flex items-center gap-2">
             <User className="h-5 w-5 text-gray-600" />
             <h2 className="text-xl font-bold">Profil</h2>
@@ -38,31 +38,86 @@ export function SettingsPanel({ userFromSupabase }: SettingsPanelProps) {
             className="flex flex-col gap-4 rounded-lg border border-gray-200 bg-white p-6 md:gap-5"
           >
             <div className="grid grid-cols-1 gap-5 md:grid-cols-2 md:gap-8">
-              <Label
+             <div className="flex flex-col gap-3">
+               <Label
                 htmlFor="firstname"
-                className="flex flex-1 flex-col items-start"
               >
                 Imię
-                <Input
+              </Label>
+              <Input
                   defaultValue={user.firstname}
                   type="text"
                   name="firstname"
                   id="firstname"
                 />
-              </Label>
-              <Label
+             </div>
+              <div className="flex flex-col gap-3">
+                <Label
                 htmlFor="lastname"
-                className="flex flex-1 flex-col items-start"
               >
                 Nazwisko
-                <Input
+              </Label>
+              <Input
                   defaultValue={user.lastname}
                   type="text"
                   name="lastname"
                   id="lastname"
                 />
-              </Label>
+              </div>
             </div>
+            <div className="flex flex-col gap-3">
+              <Label className="flex flex-col items-start">
+              Email
+            </Label>
+            <Input
+                defaultValue={user.email}
+                type="text"
+                name="email"
+                id="email"
+                disabled
+                className="cursor-not-allowed border-0 bg-gray-100"
+              />
+              <p className="pt-2 text-sm text-gray-500">
+              Email może zmienić tylko administrator
+            </p>
+            </div>
+            
+            <div className="flex flex-col gap-3">
+            <Label className="flex flex-col items-start">
+              Rola dodatkowa
+            </Label>
+            <Input
+                defaultValue={setAdditionalRoles()}
+                type="text"
+                name="additional_roles"
+                id="additional_roles"
+                disabled
+                className="cursor-not-allowed border-0 bg-gray-100"
+              />
+               <p className="pt-2 text-sm text-gray-500">
+              Rolę może zmienić tylko administrator
+            </p>
+            </div>
+           
+           
+            <Button
+              type="submit"
+              className="cursor-pointer self-end bg-(--second-color) text-black hover:bg-(--second-color-hover)"
+            >
+              Zapisz zmiany
+            </Button>
+          </form>
+        </section>
+
+         <section>
+          <div className="mb-4 flex items-center gap-2">
+            <Lock className="h-5 w-5 text-gray-600" />
+            <h2 className="text-xl font-bold">Resetuje hasło</h2>
+          </div>
+          <form
+            action=""
+            className="flex flex-col gap-4 rounded-lg border border-gray-200 bg-white p-6 md:gap-5"
+          >
             <Label className="flex flex-col items-start">
               Email
               <Input
@@ -95,7 +150,7 @@ export function SettingsPanel({ userFromSupabase }: SettingsPanelProps) {
               type="submit"
               className="cursor-pointer self-end bg-(--second-color) text-black hover:bg-(--second-color-hover)"
             >
-              Zapisz zmiany
+              Resetuj hasło
             </Button>
           </form>
         </section>
