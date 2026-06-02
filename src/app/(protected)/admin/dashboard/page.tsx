9b37b3e7eac7aa7b5031e79252a3d63a85427e17
DepleyPuma <szymon.szarek1@wp.pl>
2026-06-02 14:33:30 +0200

    implement async data fetching in AdminDashboardPage and update StatisticCard to display dynamic values

--- src/app/(protected)/admin/dashboard/page.tsx
+++ src/app/(protected)/admin/dashboard/page.tsx
@@ -1,14 +1,28 @@
 import StatisticCard from "@/components/admin/StatisticCard";
+import { getDashboardData } from "@/utils/actions";
 import { statCards } from "@/utils/utils";
-import React from "react";
 
-function AdminDashboradPage() {
+async function AdminDashboradPage() {
+  const results = await getDashboardData();
+
+  if (!results.success) {
+    console.log("Nie udało się pobrać danych");
+    return;
+  }
+
+  const dashboardData = results.data as Record<string, number>;
+
+  console.log(dashboardData);
   return (
     <div className="mx-auto max-w-7xl flex-1 px-4 pt-12">
       <h1 className="mb-8 text-3xl font-bold">Dashboard</h1>
       <div className="mb-8 grid grid-cols-1 gap-4 pr-4 sm:grid-cols-2 lg:grid-cols-4">
-        {statCards.map((card, index) => (
-          <StatisticCard card={card} key={index} />
+        {statCards.map((card) => (
+          <StatisticCard
+            key={card.key}
+            card={card}
+            value={dashboardData[card.key] ?? 0}
+          />
         ))}
       </div>
     </div>
